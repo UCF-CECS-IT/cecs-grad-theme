30caa3d0012256cd1db85abc4533b8265b666555
Paul <paulgsepulveda@gmail.com>
2021-08-31 09:07:31 -0400

    initial setup

--- includes/config.php
+++ includes/config.php
@@ -2,11 +2,11 @@
 /**
  * Handle all theme configuration here
  **/
-namespace MyProject\Theme\Includes\Config;
+namespace CECSGrad\Theme\Includes\Config;
 
 
-define( 'MYPROJECT_THEME_URL', get_stylesheet_directory_uri() );
-define( 'MYPROJECT_THEME_STATIC_URL', MYPROJECT_THEME_URL . '/static' );
-define( 'MYPROJECT_THEME_CSS_URL', MYPROJECT_THEME_STATIC_URL . '/css' );
-define( 'MYPROJECT_THEME_JS_URL', MYPROJECT_THEME_STATIC_URL . '/js' );
-define( 'MYPROJECT_THEME_IMG_URL', MYPROJECT_THEME_STATIC_URL . '/img' );
+define( 'CECS_GRAD_URL', get_stylesheet_directory_uri() );
+define( 'CECS_GRAD_STATIC_URL', CECS_GRAD_URL . '/static' );
+define( 'CECS_GRAD_CSS_URL', CECS_GRAD_STATIC_URL . '/css' );
+define( 'CECS_GRAD_JS_URL', CECS_GRAD_STATIC_URL . '/js' );
+define( 'CECS_GRAD_IMG_URL', CECS_GRAD_STATIC_URL . '/img' );
